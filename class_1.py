754e379f5bb45193bd3412f52ff79966f0b8c2b2
duckl <ej314169255@gmail.com>
2025-05-02 19:00:12 +0800

    make classes

---
+++ class_1.py
@@ -0,0 +1,29 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
+"""класс Mentor должен стать родительским классом, 
+а от него нужно реализовать наследование классов Lecturer (лекторы) и Reviewer
+ (эксперты, проверяющие домашние задания). Очевидно, имя, фамилия 
+ и список закрепленных курсов логично реализовать на уровне родительского класса"""
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer:
+    def __init__(self, Mentor, som):
+        pass
+
+class Reviewer:
+    def __init__(self, Mentor, som):
+        pass
