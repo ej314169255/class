ba844cc1958380cb2d1a10afd9f4e732e705dd3e
ej314169255 <107900694+ej314169255@users.noreply.github.com>
2025-05-17 21:43:13 +0800

    Update class_2.py

--- class_2.py
+++ class_2.py
@@ -1,4 +1,21 @@
 from datetime import datetime
+import time
+""" The datetime module is used to commit to the dictionary.
+    with the name of the person who gave the grade, the name of the subject
+    for the uniqueness of recording and analysis
+    
+    time is used to stimulate delay
+    at the time of the assessment
+
+"""
+
+def average_of_dict(b):
+    remember_grades = b
+    counter = 0
+    for key, value in remember_grades.items():
+        counter += value
+    return round(counter/len(remember_grades), 2)
+
 
 class Student:
     def __init__(self, name, surname, gender):
@@ -9,53 +26,57 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def add_courses(self, course_name):
-        self.finished_courses.append(course_name)
+    def add_courses(self, course):
+        self.finished_courses.append(course)
 
-    def level(self, course, vol):
-        if course in self.courses_in_progress:
-            return {take_grades(self.name, self.surname, course): vol}
-        else:
-            return {take_grades(self.name, self.surname,course): 'уточните запрос'}
+    def level(self, evaluated, course, vol):
+        if course in self.courses_in_progress and course in evaluated.courses_attached:
+            evaluated.grades[self.name, self.surname, course, f"{datetime.now()}"] = vol
 
-"""выставлять студентам оценки за домашние задания. 
-   Теперь это могут делать только Reviewer (реализуйте такой метод)!
-   А что могут делать лекторы? Получать оценки за лекции от студентов :)
-   Реализуйте метод выставления оценок лекторам у класса Student
-   (оценки по 10-балльной шкале, хранятся в атрибуте-словаре у Lecturer,
-   в котором ключи – названия курсов, а значения – списки оценок).
-   Лектор при этом должен быть закреплен за тем курсом, на который записан студент."""
+    def calculate_grades(self, grades):
+        return average_of_dict(grades)
 
-def take_grades(name, surname, course):
-    """
-    Формирование ключа словаря выставленных оценок
-    где name, surname кто выставляет
-    """
-    return (name, surname, course, f"{datetime.now()}")
-print(take_grades('Roy', 'Tead', 'php'))
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-
-class Lecturer(Mentor):
-    def __init__(self, name, surname, some_val):
-        super().__init__(name, surname)
-        self.some_val = some_val
         self.grades = {}
 
-class Reviewer(Mentor):
-    def __init__(self, name, surname, some_val):
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.some_val = some_val
-    
-    def level(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[take_grades(self.name, self.surname, course)] += [grade]
-            else:
-                student.grades[take_grades(self.name, self.surname, course)] = [grade]
-        else:
-            return 'уточните запрос'
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname, cur_example_class):
+        super().__init__(name, surname)
+        self.cur_example_class = cur_example_class
+
+    def level(self, evaluated, course, vol):
+        if isinstance(evaluated, self.cur_example_class) and \
+                course in self.courses_attached and \
+                course in evaluated.courses_in_progress:
+            evaluated.grades[self.name, self.surname, course, f"{datetime.now()}"] = vol
+
+
+best_student = Student('Ruoy', 'Eman', 'man')
+best_student.courses_in_progress += ['R', 'Python']
+
+cool_mentor = Reviewer('Some', 'Buddy', Student)
+cool_mentor.courses_attached = ['R', 'Python']
+
+cool_mentor.level(best_student, 'R', 10)
+time.sleep(0.001)
+cool_mentor.level(best_student, 'R', 10)
+time.sleep(0.001)
+cool_mentor.level(best_student, 'Python', 5)
+
+lecturer = Lecturer('Том', 'Сойер')
+lecturer.courses_attached =['Python']
+best_student.level(lecturer, 'Python',7)
+time.sleep(0.000005)
+best_student.level(lecturer, 'Python', 8)
+print(best_student.courses_in_progress)
+print(best_student.calculate_grades(best_student.grades))
+print(lecturer.grades)
